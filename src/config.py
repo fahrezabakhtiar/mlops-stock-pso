508c65f5adc6ee308ff9b18acb05691926b21db1
Fourze2022 <mastersatria2014@gmail.com>
2025-06-12 13:08:19 +0700

    Add code docummentation

--- src/config.py
+++ src/config.py
@@ -1,1 +1,6 @@
+# Konfigurasi global untuk daftar kode saham yang akan diproses dalam pipeline
+# Daftar ini dapat diubah sesuai kebutuhan, pastikan menggunakan kode saham dari BEI tanpa akhiran ".JK" 
+# karena akhiran tersebut akan ditambahkan secara otomatis pada tahapan extract.
+
+# List ticker saham dari Bursa Efek Indonesia
 TICKERS = ['BBRI', 'BMRI', 'BBCA', 'BBNI','GOTO','TLKM']
