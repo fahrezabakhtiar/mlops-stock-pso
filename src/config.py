f632b2e5c43b80aa461a718a78fc3b162608b642
andika0001 <csandika001@gmail.com>
2025-06-12 15:30:56 +0700

    nambahin ticker global ke config.py

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 # Konfigurasi global untuk daftar kode saham yang akan diproses dalam pipeline
-# Daftar ini dapat diubah sesuai kebutuhan, pastikan menggunakan kode saham dari BEI tanpa akhiran ".JK" 
+# Daftar ini dapat diubah sesuai saham yang ingin di predict
 # karena akhiran tersebut akan ditambahkan secara otomatis pada tahapan extract.
 
-# List ticker saham dari Bursa Efek Indonesia
-TICKERS = ['BBRI', 'BMRI', 'BBCA', 'BBNI','GOTO','TLKM']
+# List ticker saham global
+TICKERS = ['BBRI.JK', 'BMRI.JK', 'BBCA.JK', 'BBNI.JK','GOTO.JK','TLKM.JK', 'UNVR.JK', 'ASII.JK', 'ANTM.JK', 'PGAS.JK', 'AAPL', 'TSLA', 'MSFT']
