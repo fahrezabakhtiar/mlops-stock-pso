525a00bfdbada10d1b19595533c623184ebaf0d6
Fahreza Bakhtiar <142299115+fahrezabakhtiar@users.noreply.github.com>
2025-06-17 10:18:55 +0700

    Remove 'GOTO.JK', 'TLKM.JK', 'UNVR.JK', 'ASII.JK', 'ANTM.JK', 'PGAS.JK', 'AAPL', 'TSLA', 'MSFT' from ticker list

--- src/config.py
+++ src/config.py
@@ -3,4 +3,4 @@
 # karena akhiran tersebut akan ditambahkan secara otomatis pada tahapan extract.
 
 # List ticker saham global
-TICKERS = ['BBRI.JK', 'BMRI.JK', 'BBCA.JK', 'BBNI.JK','GOTO.JK','TLKM.JK', 'UNVR.JK', 'ASII.JK', 'ANTM.JK', 'PGAS.JK', 'AAPL', 'TSLA', 'MSFT']
+TICKERS = ['BBRI.JK', 'BMRI.JK', 'BBCA.JK', 'BBNI.JK']
